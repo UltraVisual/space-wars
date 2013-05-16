42947866f8e588a4224c56fb8444369d3b7324f0
Shane Johnson <shanej@ultravisual.co.uk>
2013-05-16 22:55:04 +0100

    adding screens

--- src/js/ultravisual/screens/main-menu.js
+++ src/js/ultravisual/screens/main-menu.js
@@ -1,18 +1,25 @@
 define(["ultravisual/levels/level-one"], function (LevelOne) {
-    var game;
+    var game, hasClicked = false;
     var MainMenu = function (_game) {
         game = _game;
     }
 
     MainMenu.prototype = {
         init: function () {
-            game.switchState(LevelOne)
+            game.camera.backgroundColor = 'rgb(95,95,95)';
+            game.loader.addImageFile('main-title', 'media/title.png');
+            game.loader.load();
         },
         create: function () {
-
+            var createSprite = game.createSprite(800, 480, 'main-title');
+            console.log("Shane: main-menu.js :: create :: createSprite", createSprite);
         },
         update: function () {
-
+            if (game.input.mouse.isDown && hasClicked == false){
+                console.log("Shane: main-menu.js :: update :: hasClicked", hasClicked);
+                hasClicked = true;
+                game.switchState(LevelOne)
+            }
         },
         render: function () {
 
